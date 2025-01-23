fa7e5ec05f026fb492fb3b9a1e296a50a42bd602
zander931 <zanderackevic@icloud.com>
2025-01-23 11:59:50 +0000

    Explore streamlit and wireframe design

---
+++ pipeline/truck_transactions_trial.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+
+@st.cache_data
+def load_data():
+
+    data = pd.read_csv('data/truck_hist_cleaned.csv')
+    data['timestamp'] = pd.to_datetime(data['timestamp'])
+    data['truck_id'] = pd.to_numeric(data['truck_id'])
+    data['total'] = pd.to_numeric(data['total'])
+    return data
+
+
+st.title('Total number of transactions for each truck')
+data = load_data()
+
+transaction_counts = data.groupby(
+    'truck_id').size().reset_index(name='num_transactions')
+
+st.bar_chart(transaction_counts)
